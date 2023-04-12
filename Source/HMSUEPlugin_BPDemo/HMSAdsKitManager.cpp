6863493fa47f63af2937f2c5264358de60445020
bny <eymen_15@hotmail.com>
2023-04-13 01:07:39 +0300

    compilable

--- Source/HMSUEPlugin_BPDemo/HMSAdsKitManager.cpp
+++ Source/HMSUEPlugin_BPDemo/HMSAdsKitManager.cpp
@@ -12,11 +12,11 @@ AHMSAdsKitManager::AHMSAdsKitManager()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	Ads::setListener(new AdsKitListener());
+	//Ads::setListener(new AdsKitListener());
 
-	Ads::loadBannerAd("testw6vs28auh3", BannerPosition::POSITION_BOTTOM, BannerSize::BANNER_SIZE_320_50);
-	Ads::loadInterstitialAd("testb4znbuh3n2");
-	Ads::loadRewardAd("testx9dtjwj8hp");
+	//Ads::loadBannerAd("testw6vs28auh3", BannerPosition::POSITION_BOTTOM, BannerSize::BANNER_SIZE_320_50);
+	//Ads::loadInterstitialAd("testb4znbuh3n2");
+	//Ads::loadRewardAd("testx9dtjwj8hp");
 }
 
 // Called when the game starts or when spawned
